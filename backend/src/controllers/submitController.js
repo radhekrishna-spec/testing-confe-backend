d8934802a8c30cfd33dbf4830b54a0e6bea80b1e
Ashu Singh <singhaashu271@gmail.com>
2026-04-05 13:09:46 +0530

    latest update

--- backend/src/controllers/submitController.js
+++ backend/src/controllers/submitController.js
@@ -20,6 +20,12 @@ exports.submitConfession = async (req, res) => {
 
 exports.postConfessionNow = async (req, res) => {
   try {
+    store.set('state_19', 'APPROVED');
+    store.set('images_19', [
+      'https://lh3.googleusercontent.com/d/1cj_hjaR-I41KG2hrOZUhSquCHySLtR5s',
+    ]);
+    store.set('caption_19', 'Confession #19');
+
     const result = await processApprovedQueue();
 
     res.status(200).json({
