56e76cdd7cebf030a22d0cc4c9a070b33602b0ef
Ashu Singh <singhaashu271@gmail.com>
2026-04-07 23:05:07 +0530

    production update

--- frontend/admin-ui/src/pages/AdminDashboardPage.jsx
+++ frontend/admin-ui/src/pages/AdminDashboardPage.jsx
@@ -11,7 +11,9 @@ export default function AdminDashboardPage() {
   const [selectedConfession, setSelectedConfession] = useState(null);
 
   useEffect(() => {
-    fetch('https://confession-admin-ui.onrender.com/api/admin/confessions')
+    fetch(
+      'https://confession-saas-node-v7-webhookless-max.onrender.com/api/admin/confessions',
+    )
       .then((res) => res.json())
       .then((data) => setConfessions(data))
       .catch((err) => console.error(err));
