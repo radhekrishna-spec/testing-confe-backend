2f71727afa2e1e75fa35501242fb055a82229f58
Ashu Singh <singhaashu271@gmail.com>
2026-04-18 04:11:13 +0530

    update

--- backend/src/modules/confession/helpers/slideTextFit.js
+++ backend/src/modules/confession/helpers/slideTextFit.js
@@ -3,27 +3,40 @@ function autoFitTextConfig(text, type = 'confession') {
   // 📌 SHAYARI MODE
   // =========================
   if (type === 'shayari') {
-    const lines = text.split('\n').length;
+    const textLength = text.length;
 
-    let fontSize = 90;
-    let lineSpacing = 120;
+    let fontSize;
+    let lineSpacing = 110;
 
-    if (lines <= 2) {
-      fontSize = 110;
-      lineSpacing = 110;
-    } else if (lines <= 4) {
-      fontSize = 95;
-      lineSpacing = 115;
-    } else if (lines <= 6) {
-      fontSize = 85;
-      lineSpacing = 120;
-    } else if (lines <= 8) {
-      fontSize = 75;
-      lineSpacing = 125;
-    } else {
-      fontSize = 65;
-      lineSpacing = 130;
-    }
+    if (textLength <= 20) lineSpacing = 75;
+    else if (textLength <= 30) lineSpacing = 82;
+    else if (textLength <= 40) lineSpacing = 88;
+    else if (textLength <= 50) lineSpacing = 92;
+    else if (textLength <= 60) lineSpacing = 96;
+    else if (textLength <= 84) lineSpacing = 95;
+
+    if (textLength <= 50) fontSize = 50;
+    else if (textLength <= 60) fontSize = 48;
+    else if (textLength <= 84) fontSize = 46;
+    else if (textLength <= 104) fontSize = 44;
+    else if (textLength <= 120) fontSize = 42;
+    else if (textLength <= 144) fontSize = 40;
+    else if (textLength <= 170) fontSize = 38;
+    else if (textLength <= 180) fontSize = 36;
+    else if (textLength <= 209) fontSize = 34;
+    else if (textLength <= 220) fontSize = 32;
+    else if (textLength <= 240) fontSize = 30;
+    else if (textLength <= 264) fontSize = 28;
+    else if (textLength <= 299) fontSize = 26;
+    else if (textLength <= 336) fontSize = 24;
+    else if (textLength <= 350) fontSize = 22;
+    else if (textLength <= 405) fontSize = 20;
+    else if (textLength <= 464) fontSize = 18;
+    else if (textLength <= 510) fontSize = 16;
+    else if (textLength <= 576) fontSize = 14;
+    else if (textLength <= 627) fontSize = 12;
+    else if (textLength <= 665) fontSize = 10;
+    else fontSize = 8;
 
     return {
       fontSize,
