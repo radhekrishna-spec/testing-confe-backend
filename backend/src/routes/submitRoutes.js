04aba0058f23474ea8d0db442a62b31b65bfcbca
Ashu Singh <singhaashu271@gmail.com>
2026-04-18 14:22:42 +0530

    update

--- backend/src/routes/submitRoutes.js
+++ backend/src/routes/submitRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const identifyCollege = require('../../middleware/identifyCollege');
+const identifyCollege = require('../middleware/identifyCollege');
 const {
   submitConfession,
   postConfessionNow,
