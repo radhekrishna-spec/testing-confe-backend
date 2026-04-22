6d2973c46ca86c885d60d77fbdeccb593be95d89
Ashu Singh <singhaashu271@gmail.com>
2026-04-22 12:21:39 +0530

    update

--- frontend/admin-ui/src/main.jsx
+++ frontend/admin-ui/src/main.jsx
@@ -1,7 +1,10 @@
+import axios from 'axios';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+
+axios.defaults.withCredentials = true;
 const originalFetch = window.fetch;
 
 window.fetch = (url, options = {}) => {
