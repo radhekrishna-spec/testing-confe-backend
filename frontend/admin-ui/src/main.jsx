f003b3840a7a09545cc0c440dcfee017883453ba
Ashu Singh <singhaashu271@gmail.com>
2026-04-22 12:21:06 +0530

    update

--- frontend/admin-ui/src/main.jsx
+++ frontend/admin-ui/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+const originalFetch = window.fetch;
+
+window.fetch = (url, options = {}) => {
+  return originalFetch(url, {
+    credentials: 'include',
+    ...options,
+  });
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
